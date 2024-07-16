0b880e7e84435be218846c806673db1c731f82de
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-07-16 18:48:47 -0500

    update fim_tool

--- scripts/install_service.sh
+++ scripts/install_service.sh
@@ -20,7 +20,7 @@ After=network.target
 
 [Service]
 User=root
-ExecStart=python3 /usr/local/bin/fim_tools/automate.py
+ExecStart=python3 /usr/local/bin/fim_tool/automate.py
 Restart=always
 
 [Install]
